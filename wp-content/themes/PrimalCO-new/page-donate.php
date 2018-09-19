54a33371540683dcfcae45c261f198ba5c7d5ac1
Daniel van der Velden <danielvandervelden@Daniels-MacBook-Pro.local>
2018-09-19 18:25:27 +0200

    Removed unnecessary round, does this fix it?

--- wp-content/themes/PrimalCO-new/page-donate.php
+++ wp-content/themes/PrimalCO-new/page-donate.php
@@ -89,7 +89,7 @@ var donationAmount;
         transactions: [
           {
             amount: {
-              total: Math.round(donationAmount),
+              total: donationAmount,
               currency: 'EUR'
             }
           }
