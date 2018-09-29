5481cc45ea7439d228bafc953aee1c8a77610639
Daniel van der Velden <danielvandervelden@Daniels-MacBook-Pro.local>
2018-09-29 23:37:26 +0200

    Voting should be fixed now!

--- wp-content/themes/PrimalCO-new/database/voteresult.php
+++ wp-content/themes/PrimalCO-new/database/voteresult.php
@@ -4,13 +4,14 @@
 Template Name: voteresult
 */
 
-$username = $_GET['user'];
-$token = $_GET['token'];
+$token = $_GET['custom'];
 
 require 'db.php';
 
-$result = $mysqli->query("SELECT * FROM accounts WHERE Username='$username'");
+$result = $mysqli->query("SELECT * FROM accounts WHERE Token='$token'");
 $user = $result->fetch_assoc();
+
+$username = $user['Username'];
 
 $lastVote = $user['LastVote'];
 $allowedVote = date('Y-m-d H:i:s',strtotime('+12 hour',strtotime($lastVote)));
@@ -21,12 +22,10 @@ $dt = new DateTime("now", new DateTimeZone($tz)); //first argument "must" be a s
 $dt->setTimestamp($timestamp); //adjust the object to correct timestamp
 $currentTime = $dt->format('Y-m-d H:i:s');
 
-$currentVotePoints = $user['VotePoints'];
-$newVotePoints = $currentVotePoints + 1;
+if($allowedVote < $currentTime) {
+    $currentVotePoints = $user['VotePoints'];
+    $newVotePoints = $currentVotePoints + 1;
 
-$updateVotePoints = $mysqli->query("UPDATE accounts SET VotePoints = '$newVotePoints' WHERE Username = '$username'");
-$updateLastVoted = $mysqli->query("UPDATE accounts SET LastVote = '$currentTime' WHERE Username = '$username'");
-
-if($user['Token'] == $token && $username === $user['Username'] && $allowedVote < $currentTime) {
-
+    $updateVotePoints = $mysqli->query("UPDATE accounts SET VotePoints = '$newVotePoints' WHERE Username = '$username'");
+    $updateLastVoted = $mysqli->query("UPDATE accounts SET LastVote = '$currentTime' WHERE Username = '$username'");
  }
--- wp-content/themes/PrimalCO-new/header.php
+++ wp-content/themes/PrimalCO-new/header.php
@@ -121,7 +121,7 @@ if($logged_in) {
             <?php if($username) : ?>
             <div class="xtremetop100">
                 <h2 class="white">Vote for us!</h2>
-                <a href='<?php echo "http://www.xtremetop100.com/in.php?site=1132324901&user=" . $username . "&token=" . $user['Token'] ?>'>
+                <a href='<?php echo "http://www.xtremetop100.com/in.php?site=1132324901&custom=" . $token ?>'>
                 <img src="/wp-content/themes/PrimalCO-new/assets/images/votenew.jpg" alt="private server"></a>
             </div>
             <?php endif; ?>     
