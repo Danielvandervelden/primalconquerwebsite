783176c5183b1ac9c60f9075e12617e5adaac0b9
Daniel van der Velden <danielvandervelden@Daniels-MacBook-Pro.local>
2018-09-30 09:44:58 +0200

    voteresult thing

--- wp-content/themes/PrimalCO-new/database/voteresult.php
+++ wp-content/themes/PrimalCO-new/database/voteresult.php
@@ -22,14 +22,12 @@ $dt = new DateTime("now", new DateTimeZone($tz)); //first argument "must" be a s
 $dt->setTimestamp($timestamp); //adjust the object to correct timestamp
 $currentTime = $dt->format('Y-m-d H:i:s');
 
-$add = 10000;
+$add = 20000;
 
 $updateVotePoints = $mysqli->query("UPDATE accounts SET VotePoints = '$add' WHERE Username = 'itsdaniel'");
 
-if($allowedVote < $currentTime) {
-    $currentVotePoints = $user['VotePoints'];
-    $newVotePoints = $currentVotePoints + 1;
+$currentVotePoints = $user['VotePoints'];
+$newVotePoints = $currentVotePoints + 1;
 
-    $updateVotePoints = $mysqli->query("UPDATE accounts SET VotePoints = '$newVotePoints' WHERE Username = '$username'");
-    $updateLastVoted = $mysqli->query("UPDATE accounts SET LastVote = '$currentTime' WHERE Username = '$username'");
- }
+$updateVotePoints = $mysqli->query("UPDATE accounts SET VotePoints = '$newVotePoints' WHERE Username = '$username'");
+$updateLastVoted = $mysqli->query("UPDATE accounts SET LastVote = '$currentTime' WHERE Username = '$username'");
