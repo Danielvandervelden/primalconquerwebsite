386802e9093b375fe6a7fcc3ba5d99bd327e3c38
Daniel van der Velden <danielvandervelden@Daniels-MacBook-Pro.local>
2018-10-06 10:20:24 +0200

    Now people need to be logged in

--- wp-content/themes/PrimalCO-new/page-transfer.php
+++ wp-content/themes/PrimalCO-new/page-transfer.php
@@ -3,11 +3,13 @@ get_header();
 require 'database/db.php';
 
 $servers = Array('ConquerGods');
+$loggedIn = $_SESSION['logged-in'];
+?> 
 
-?>
 
 <div class="main-content-container padding-top padding-bottom">
 
+    <?php if($loggedIn) { ?>
     <form action="/transfer" method="POST" class="margin-center">
         <div class="spacing-top spacing-bottom">
             <p class="white"><?php the_content(); ?></p>
@@ -33,6 +35,10 @@ $servers = Array('ConquerGods');
         </select>
         <button name="submit_transfer" class="btn spacing-top" type="submit">TRANSFER ME!</button>
     </form>
+
+    <?php } else { ?>
+        <p class="spacing-left spacing-right spacing-top spacing-bottom">Please log in in order to transfer your account from another server!</p>
+    <?php } ?>
 </div>
 
 
